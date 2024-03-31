1e2c11d7a0ef826c7d173ac78350ff677bceda51
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-03-31 22:19:13 +0000

    exemplo08

--- Curso-Udemy-Nelio/src/exercicios/Aplication/Program.java
+++ Curso-Udemy-Nelio/src/exercicios/Aplication/Program.java
@@ -2,13 +2,38 @@ package exercicios.Aplication;
 
 import java.util.Scanner;
 
+import exercicios.Aplication.entities.Products;
+
 public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter radius: ");
-        
-        
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        Products product = new Products(name, price);
+
+        product.setName("Computador ");
+        System.out.println("Upadated name: "+ product.getName());
+        product.setPrice(1200.0);
+        System.out.println("Upadated data: "+ product.getPrice());
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
         
         sc.close();
     }
